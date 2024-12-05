372b11e97246ceac20fa1c5a9c181532aae2f27d
Kulvir Singh <kulvirs734@gmail.com>
2024-12-05 17:37:32 +0530

    kyootiep2 d52s

--- 2024/day5/sol.go
+++ 2024/day5/sol.go
@@ -9,50 +9,78 @@ func Solution() {
 	ip := stl.ReadFile("input.txt")
 
 	orders := [][]int{}
-	for i, line := range ip {
-		if len(line) == 0 {
-			ip = ip[i+1:]
-			break
-		}
-		orders = append(orders, stl.IntsFromString(ip[i]))
-	}
 	jobs := [][]int{}
-	for _, job := range ip {
-		jobs = append(jobs, stl.IntsFromString(job))
+	isorder := true
+	for i := range ip {
+		if len(ip[i]) == 0 {
+			isorder = false
+			continue
+		}
+		if isorder {
+			orders = append(orders, stl.IntsFromString(ip[i]))
+		} else {
+			jobs = append(jobs, stl.IntsFromString(ip[i]))
+		}
 	}
 
-	before := getorders(orders)
-	inorderSum := inorder(jobs, before)
+	orderMap := orderMap(orders)
+
+	inorderSum := 0
+	outoforderSum := 0
+	for _, job := range jobs {
+		if isinorder(job, orderMap) {
+			inorderSum += job[len(job)/2]
+		} else {
+			outoforderSum += makeInorder(job, orderMap)
+		}
+	}
 	fmt.Println(inorderSum)
+	fmt.Println(outoforderSum)
 }
 
-func inorder(jobs [][]int, before map[int]map[int]bool) int {
-	middleSum := 0
-	for _, job := range jobs {
-		outoforder := false
-		for i, page := range job {
-			for j := i + 1; j < len(job); j++ {
-				if ok := before[page][job[j]]; ok {
-					outoforder = true
-					break
-				}
+func isinorder(job []int, orderMap map[int]map[int]bool) bool {
+	for i, page := range job {
+		for j := i + 1; j < len(job); j++ {
+			if ok := orderMap[page][job[j]]; ok {
+				return false
 			}
 		}
-		if !outoforder {
-			middleSum += job[len(job)/2]
-		}
 	}
-	return middleSum
+	return true
 }
 
-func getorders(orders [][]int) map[int]map[int]bool {
+func makeInorder(job []int, orderMap map[int]map[int]bool) int {
+	ordered := make([]int, len(job)) // jobs in ordered sequence
+	for i, num := range job {
+		before := 0
+		for j := 0; j < len(job); j++ {
+			if j == i {
+				continue
+			}
+			// count of elements that comes before ith elemnt
+			// correct pos of i'th element will be num of elements [before] + 1
+			if _, ok := orderMap[num][job[j]]; ok {
+				before++
+			}
+		}
+		ordered[before] = job[i]
+		if ordered[len(job)/2] != 0 {
+			return ordered[len(job)/2]
+		}
+	}
+	return ordered[len(job)/2]
+}
+
+func orderMap(orders [][]int) map[int]map[int]bool {
+	// 47|53 -> 47 comes before 53
 	before := make(map[int]map[int]bool)
 	for _, ord := range orders {
 		first, second := ord[0], ord[1]
-		// before[second] = append(before[second], first)
 		if before[second] == nil {
 			before[second] = make(map[int]bool)
 		}
+		// 53 : {{47: true}...}
+		// elements coming before 53
 		before[second][first] = true
 	}
 	return before
