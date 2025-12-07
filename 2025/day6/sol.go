64d234253905c86e2a0ce7f76f1946a9e8b16252
Kulvir Singh <kulvirs734@gmail.com>
2025-12-07 17:46:22 +0530

    day-6(2): funky RTL maths problem

--- 2025/day6/sol.go
+++ 2025/day6/sol.go
@@ -2,6 +2,9 @@ package day6
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 
 	stl "aoc/stl"
 )
@@ -12,6 +15,9 @@ func Solution() {
 	// ip := stl.ReadFile("example.txt")
 	nums, ops := getNumsAndOps(ip)
 	total := solveProblems(nums, ops)
+	fmt.Println(total)
+
+	total = solveFunkyProblem(ip)
 	fmt.Println(total)
 }
 
@@ -32,6 +38,68 @@ func solveProblems(nums [][]int, ops []string) (total int) {
 			total += acc
 		}
 	}
+	return total
+}
+
+func solveFunkyProblem(ip []string) (total int) {
+	n := len(ip)
+
+	opsRow := ip[n-1]
+	numsRows := ip[:n-1]
+	nr := len(numsRows)
+
+	op := ""
+	numsForCalc := []int{}
+	for col := range len(opsRow) {
+		if opsRow[col] != ' ' {
+			switch op {
+			case "+":
+				acc := 0
+				for _, num := range numsForCalc {
+					acc += num
+				}
+				total += acc
+			case "*":
+				acc := 1
+				for _, num := range numsForCalc {
+					acc *= num
+				}
+				total += acc
+			}
+
+			op = string(opsRow[col])
+			numsForCalc = []int{}
+		}
+
+		var sb strings.Builder
+		for row := range nr {
+			d := numsRows[row][col]
+			if unicode.IsDigit(rune(d)) {
+				sb.WriteByte(numsRows[row][col])
+			}
+		}
+
+		if sb.Len() > 0 {
+			num, _ := strconv.ParseInt(sb.String(), 10, 64)
+			numsForCalc = append(numsForCalc, int(num))
+		}
+	}
+
+	switch op {
+	case "+":
+		acc := 0
+		for _, num := range numsForCalc {
+			acc += num
+		}
+		total += acc
+	case "*":
+		acc := 1
+		for _, num := range numsForCalc {
+			acc *= num
+		}
+		total += acc
+	}
+
 	return total
 }
 
